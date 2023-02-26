f228eb2da9f7225a86c670a490dc707092168120
rajeswari69 <rajeswarigottipalli69@gmail.com>
2023-02-26 06:32:03 +0530

    added springboot application

---
+++ src/test/java/com/example/DockerAssignment/DockerAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
